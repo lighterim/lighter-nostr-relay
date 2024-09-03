1bad3652dff1e13ef63aa8ea60a00406f57af4f7
Dust <dust@live.com>
2024-09-03 12:51:09 +0700

    genericTagQuery bipredicate

--- src/main/java/com/prosilion/superconductor/entity/join/subscriber/FilterGenericTagPlugin.java
+++ src/main/java/com/prosilion/superconductor/entity/join/subscriber/FilterGenericTagPlugin.java
@@ -4,21 +4,45 @@ import com.prosilion.superconductor.pubsub.AddNostrEvent;
 import nostr.base.ElementAttribute;
 import nostr.base.GenericTagQuery;
 import nostr.event.BaseTag;
-import nostr.event.NIP77Event;
-import nostr.event.impl.Filters;
-import nostr.event.impl.GenericEvent;
-import nostr.event.impl.GenericTag;
-import nostr.event.tag.*;
+import nostr.event.Kind;
+import nostr.event.impl.*;
+import nostr.event.tag.MakeTag;
+import nostr.event.tag.PaymentTag;
+import nostr.event.tag.QuoteTag;
+import nostr.event.tag.TokenTag;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
-import java.util.stream.Collectors;
+
+import static nostr.event.Kind.TEXT_NOTE;
 
 @Component
-public class FilterGenericTagPlugin<T extends SubscriberFilterGenericTag> implements FilterPlugin<T>{
+public class FilterGenericTagPlugin<T extends SubscriberFilterGenericTag> implements FilterPlugin<T> {
+    private static boolean defaultPredicate(String tagName, List<String> value, GenericEvent event) {
+        List<BaseTag> baseTags = event.getTags();
+        List<GenericTag> genericTags = baseTags.stream().filter(GenericTag.class::isInstance).map(GenericTag.class::cast).toList();
+
+        if (genericTags.isEmpty()) return false;
+        List<GenericTag> targetTags = genericTags.stream().filter(it -> it.getCode() != null && it.getCode().equals(tagName)).toList();
+        if (targetTags.isEmpty()) return false;
+
+        return targetTags.stream().anyMatch(it -> {
+            List<ElementAttribute> attributes = it.getAttributes();
+            if (attributes == null || attributes.isEmpty()) {
+                return false;
+            }
+            for (ElementAttribute element : attributes) {
+                if (element != null && value.contains(element.getValue())) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
     @Override
     public String getCode() {
         return "genericTag";
@@ -29,81 +53,51 @@ public class FilterGenericTagPlugin<T extends SubscriberFilterGenericTag> implem
         return (t, u) -> {
             String tagName = t.getTagName();
             List<String> value = t.getValue();
-            if (tagName == null || value == null || value.isEmpty())
-                return true;
+            if (tagName == null || value == null || value.isEmpty()) return true;
 
-            List<BaseTag> baseTags = u.event().getTags();
-            List<GenericTag> genericTags = baseTags.stream()
-                    .filter(GenericTag.class::isInstance)
-                    .map(GenericTag.class::cast)
-                    .toList();
-
-            if (tagName.equals("side")) {
-                BaseTag makeTag = baseTags.stream().filter(it->it instanceof MakeTag).findFirst().orElse(null);
-                if (makeTag == null)
-                    return false;
-
-                MakeTag make = (MakeTag) makeTag;
-                if (value.contains(make.getSide().getSide())) {
-                    return true;
-                }
-                return false;
-            } else if(tagName.equals("symbol")){
-                BaseTag tokenTag = baseTags.stream().filter(it->it instanceof TokenTag).findFirst().orElse(null);
-                if (tokenTag == null)
-                    return false;
-
-                TokenTag token = (TokenTag) tokenTag;
-                if (value.contains(token.getSymbol())) {
-                    return true;
-                }
-
-                return false;
-            } else if(tagName.equals("currency")){
-                BaseTag quoteTag = baseTags.stream().filter(i -> i instanceof QuoteTag).findFirst().orElse(null);
-                if(quoteTag == null){
-                    return false;
-                }
-                QuoteTag quote = (QuoteTag) quoteTag;
-                if(value.contains(quote.getCurrency())){
-                    return true;
-                }
-                return false;
-            } else if(tagName.equals("payment_method")){
-                List<PaymentTag> paymentMethods = baseTags.stream().filter(i->i instanceof PaymentTag).map(PaymentTag.class::cast).toList();
-                if(paymentMethods.isEmpty()){
-                    return false;
-                }
-
-                for(PaymentTag p : paymentMethods){
-                    if(value.contains(p.getMethod())){
-                        return true;
-                    }
-                }
-                return false;
-
-            }else{
-                if (genericTags.isEmpty())
-                    return false;
-                List<GenericTag> targetTags = genericTags.stream().filter(it->it.getCode()!=null && it.getCode().equals(tagName)).toList();
-                if (targetTags.isEmpty())
-                    return false;
-
-                return targetTags.stream().anyMatch(it->{
-                    List<ElementAttribute> attributes = it.getAttributes();
-                    if (attributes == null || attributes.isEmpty()){
-                        return false;
-                    }
-                    for (ElementAttribute element: attributes) {
-                        if (element != null && value.contains(element.getValue())) {
-                            return true;
-                        }
-                    }
-                    return false;
-                });
+            GenericEvent event = u.event();
+            Kind kind = event.getKind() == null ? TEXT_NOTE : Kind.valueOf(event.getKind());
+            switch (kind) {
+                case POST_INTENT:
+                    return getBiPredicate(tagName, value, (PostIntentEvent) event);
+                case TAKE_INTENT:
+                    return getBiPredicate(tagName, value, (TakeIntentEvent) event);
+                case TRADE_MESSAGE:
+                    return getBiPredicate(tagName, value, (TradeMessageEvent) event);
+                default:
+                    return defaultPredicate(tagName, value, event);
             }
-
         };
+    }
+
+    private boolean getBiPredicate(String tagName, List<String> value, TradeMessageEvent event) {
+        return false;
+    }
+
+    private boolean getBiPredicate(String tagName, List<String> value, TakeIntentEvent event) {
+        return false;
+    }
+
+    private boolean getBiPredicate(String tagName, List<String> value, PostIntentEvent event) {
+        if (tagName.equals("side")) {
+            MakeTag makeTag = event.getSideTag();
+            return makeTag != null && value.contains(makeTag.getSide().getSide());
+        } else if (tagName.equals("symbol")) {
+            TokenTag token = event.getTokenTag();
+            return token != null && value.contains(token.getSymbol());
+        } else if (tagName.equals("currency")) {
+            QuoteTag quote = event.getQuoteTag();
+            return quote != null && value.contains(quote.getCurrency());
+        } else if (tagName.equals("payment_method")) {
+            List<PaymentTag> paymentMethods = event.getPaymentTags();
+            for (PaymentTag p : paymentMethods) {
+                if (value.contains(p.getMethod())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 
     @Override
@@ -113,7 +107,7 @@ public class FilterGenericTagPlugin<T extends SubscriberFilterGenericTag> implem
                 return null;
             }
             return filters.getGenericTagQueryList();
-        }else {
+        } else {
             return new ArrayList<>(Collections.singleton(filters.getGenericTagQuery()));
         }
     }

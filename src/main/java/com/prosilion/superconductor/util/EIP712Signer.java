d2f43e09eaea59bce20bc866a0a7abb3e2e5eee4
Dust <dust@live.com>
2025-10-19 21:41:18 +0800

    remove usdRate in PostIntent validation

--- src/main/java/com/prosilion/superconductor/util/EIP712Signer.java
+++ src/main/java/com/prosilion/superconductor/util/EIP712Signer.java
@@ -60,6 +60,7 @@ public class EIP712Signer {
         }
         try {
             String expectedAddress = eip712Tag.getWalletAddress();
+            log.info("event-id:{}, structDataJson1:{}, event1:{}", event.getId(), structuredDataJson, event);
             StructuredDataEncoder encoder = new StructuredDataEncoder(structuredDataJson);
             byte[] messageHash = encoder.hashStructuredData();
             log.info("event-id:{}, structDataJson:{}, hash:{}, event:{}", event.getId(), structuredDataJson, org.web3j.utils.Numeric.toHexString(messageHash), event);
@@ -144,7 +145,7 @@ public class EIP712Signer {
         paramsType.add(createType("currency", "bytes32"));
         paramsType.add(createType("paymentMethod", "bytes32"));
         paramsType.add(createType("payeeDetails", "bytes32"));
-        paramsType.add(createType("usdRate", "uint256"));
+//        paramsType.add(createType("usdRate", "uint256"));
         paramsType.add(createType("price", "uint256"));
 
         types.put("IntentParams", paramsType);
@@ -176,7 +177,7 @@ public class EIP712Signer {
         messageMap.put("paymentMethod", keccak256(paymentTag.getMethod()));
         messageMap.put("payeeDetails", keccak256(paymentTag.getAccount() + paymentTag.getQrCode() + paymentTag.getMemo()));
         messageMap.put("price", quoteTag.getNumber().toPlainString());
-        messageMap.put("usdRate", quoteTag.getUsdRate().intValue());
+//        messageMap.put("usdRate", quoteTag.getUsdRate().toPlainString());
 
         structuredData.put("message", messageMap);
         structuredData.put("primaryType", "IntentParams");

260db11c006f755986182135d06277d7f8195bf7
kuenne <quhaochu@foxmail.com>
2025-11-24 11:01:42 +0800

    add IntentEntity version

--- src/main/java/com/prosilion/superconductor/util/EIP712Signer.java
+++ src/main/java/com/prosilion/superconductor/util/EIP712Signer.java
@@ -462,7 +462,7 @@ public class EIP712Signer {
             // ["quote","3.221E+21","USD","1E+18",""]
             new QuoteTag(new BigDecimal("1000000000000000000"), "USD", new BigDecimal("1000000000000000000"), new BigInteger("1761237799"), "", 0),
             new MakeTag(Side.SELL, "", pk.toString(), IntentType.SIGNATURE_SELL),
-            new Permit2Tag("270178257646664", "0x5a41235a9127cd6a85e3ee3afcb41e26d50b0c020d2dc8c29ebfd294300bf0b815a555484204fff2996276fac6a4e5a485465a4fc893370a6f652b790ee19fef1b", "0xD58382f295f5c98BAeB525FAbb7FEBcCc62bc63B", "0x1e4d58c5a97ab35c614a90ab04acc78711729f18"),
+            new Permit2Tag("270178257646664", "0x5a41235a9127cd6a85e3ee3afcb41e26d50b0c020d2dc8c29ebfd294300bf0b815a555484204fff2996276fac6a4e5a485465a4fc893370a6f652b790ee19fef1b", "0xD58382f295f5c98BAeB525FAbb7FEBcCc62bc63B", "0x1e4d58c5a97ab35c614a90ab04acc78711729f18", "", "", ""),
             new LimitTag(BigDecimal.valueOf(1000000L), BigDecimal.valueOf(1000000L)),
             new PaymentTag("wechat", "dust", "wxp://f2f0in9xnsA4G_eXWBRORK63ixD6bMQcP11eKGFz1VS4Kf0", "memo")
         );
